114b90d0511521c1712a1688b9ae330c5baf05e0
Nurdiana <84701259+Nurdiananur@users.noreply.github.com>
2022-07-13 07:11:36 +0600

    widget

--- lib/module/counter/view/counter_page.dart
+++ lib/module/counter/view/counter_page.dart
@@ -7,8 +7,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:klubnikakg1/counter/counter.dart';
+
 import 'package:klubnikakg1/l10n/l10n.dart';
+import 'package:klubnikakg1/module/counter/cubit/counter_cubit.dart';
 
 class CounterPage extends StatelessWidget {
   const CounterPage({super.key});
